JDGrimes/wp-plugin-uninstall-tester

commit 497e3878a32a1e29e9c316ebb00401476c18f299

Support multisite testing.
This is the first step for making #2 possible.

Combined the common code to load WordPress remotely into a bootstrap
for the installer and simulator.

includes/wp-plugin-uninstall-unittestcase.php
@@ -69,9 +69,29 @@ abstract class WP_Plugin_Uninstall_UnitTestCase extends WP_UnitTestCase {
 	 */
 	public function setUp() {
 
+		// Create another site on multisite.
+		if ( is_multisite() ) {
+
+			// $this->factory isn't available until after setup.
+			$factory = new WP_UnitTest_Factory;
+			$this->_blog_id = $factory->blog->create();
+		}
+
 		$this->install();
 
 		parent::setUp();
+	}
+
+	/**
+	 * Clean up after the tests.
+	 *
+	 * @since 1.2.0
+	 */
+	public function tearDown() {
+
+		parent::tearDown();
+
+		wpmu_delete_blog( $this->_blog_id, true );
 	}
 
 	/**
@@ -126,6 +146,7 @@ abstract class WP_Plugin_Uninstall_UnitTestCase extends WP_UnitTestCase {
 			. ' ' . escapeshellarg( $this->plugin_file )
 			. ' ' . escapeshellarg( $this->install_function )
 			. ' ' . escapeshellarg( $this->locate_wp_tests_config() )
+			. ' ' . is_multisite()
 		);
 	}
 
@@ -147,6 +168,7 @@ abstract class WP_Plugin_Uninstall_UnitTestCase extends WP_UnitTestCase {
 			. ' ' . escapeshellarg( $this->plugin_file )
 			. ' ' . escapeshellarg( $this->simulation_file )
 			. ' ' . escapeshellarg( $this->locate_wp_tests_config() )
+			. ' ' . is_multisite()
 		);
 	}
 

bin/bootstrap.php
@@ -1,0 +1,37 @@
+<?php
+
+/**
+ * Common bootstrap code for loading WordPress.
+ *
+ * The including file needs to properly set the $config_file_path and $is_multisite
+ * variables.
+ *
+ * @package WP_Plugin_Uninstall_Tester
+ * @since 0.2.0
+ */
+
+require $config_file_path;
+
+unset( $config_file_path );
+
+$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+$_SERVER['HTTP_HOST'] = WP_TESTS_DOMAIN;
+$PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
+
+if ( $is_multisite ) {
+
+	define( 'MULTISITE', true );
+	define( 'SUBDOMAIN_INSTALL', false );
+	define( 'DOMAIN_CURRENT_SITE', WP_TESTS_DOMAIN );
+	define( 'PATH_CURRENT_SITE', '/' );
+	define( 'SITE_ID_CURRENT_SITE', 1 );
+	define( 'BLOG_ID_CURRENT_SITE', 1 );
+
+	$GLOBALS['base'] = '/';
+}
+
+unset( $is_multisite );
+
+define( 'WP_USE_THEMES', false );
+
+require ABSPATH . '/wp-settings.php';

bin/install-plugin.php
@@ -10,18 +10,9 @@
 $plugin_file      = $argv[1];
 $install_function = $argv[2];
 $config_file_path = $argv[3];
+$is_multisite     = $argv[4];
 
-require $config_file_path;
-
-unset( $config_file_path );
-
-$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
-$_SERVER['HTTP_HOST'] = WP_TESTS_DOMAIN;
-$PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
-
-define( 'WP_USE_THEMES', false );
-
-require ABSPATH . '/wp-settings.php';
+require dirname( __FILE__ ) . '/bootstrap.php';
 
 require $plugin_file;
 

bin/simulate-plugin-use.php
@@ -31,17 +31,8 @@ tests_add_filter( 'muplugins_loaded', '_wp_plugin_unintsall_tester_load_plugin_f
 
 $simulation_file  = $argv[2];
 $config_file_path = $argv[3];
+$is_multisite     = $argv[4];
 
-require $config_file_path;
-
-unset( $config_file_path );
-
-$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
-$_SERVER['HTTP_HOST'] = WP_TESTS_DOMAIN;
-$PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
-
-define( 'WP_USE_THEMES', false );
-
-require ABSPATH . '/wp-settings.php';
+require dirname( __FILE__ ) . '/bootstrap.php';
 
 require $simulation_file;

commit 44cc35a6d318806e6f8ae21d796dc79df4a631ce

Fix errors when running tests on a non-multisite install
Caused by 497e3878a32a1e29e9c316ebb00401476c18f299

includes/wp-plugin-uninstall-unittestcase.php
@@ -91,7 +91,9 @@ abstract class WP_Plugin_Uninstall_UnitTestCase extends WP_UnitTestCase {
 
 		parent::tearDown();
 
-		wpmu_delete_blog( $this->_blog_id, true );
+		if ( is_multisite() ) {
+			wpmu_delete_blog( $this->_blog_id, true );
+		}
 	}
 
 	/**
@@ -146,7 +148,7 @@ abstract class WP_Plugin_Uninstall_UnitTestCase extends WP_UnitTestCase {
 			. ' ' . escapeshellarg( $this->plugin_file )
 			. ' ' . escapeshellarg( $this->install_function )
 			. ' ' . escapeshellarg( $this->locate_wp_tests_config() )
-			. ' ' . is_multisite()
+			. ' ' . (int) is_multisite()
 		);
 	}
 
@@ -168,7 +170,7 @@ abstract class WP_Plugin_Uninstall_UnitTestCase extends WP_UnitTestCase {
 			. ' ' . escapeshellarg( $this->plugin_file )
 			. ' ' . escapeshellarg( $this->simulation_file )
 			. ' ' . escapeshellarg( $this->locate_wp_tests_config() )
-			. ' ' . is_multisite()
+			. ' ' . (int) is_multisite()
 		);
 	}
 