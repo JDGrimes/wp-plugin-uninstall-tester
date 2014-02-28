397b34f13d43c22448d9f4925a8527f9156d38bf
JDGrimes <jdg@codesymphony.co>
2014-02-28 16:18:06 -0500

    Add a constant for the path to the main directory
    This allows the tools to be more easily extended.

--- bootstrap.php
+++ bootstrap.php
@@ -9,36 +9,41 @@
  * @since 0.1.0
  */
 
-$dir = dirname( __FILE__ );
+/**
+ * The directory path of the uninstall tester tools.
+ *
+ * @since 0.2.0
+ *
+ * @type string
+ */
+define( 'WP_PLUGIN_UNINSTALL_TESTER_DIR', dirname( __FILE__ ) );
 
 /**
  * The commandline options parser.
  */
-require_once $dir . '/includes/wp-plugin-uninstall-tester-phpunit-util-getopt.php';
+require_once WP_PLUGIN_UNINSTALL_TESTER_DIR . '/includes/wp-plugin-uninstall-tester-phpunit-util-getopt.php';
 
 /**
  * General functions.
  */
-require_once $dir . '/includes/functions.php';
+require_once WP_PLUGIN_UNINSTALL_TESTER_DIR . '/includes/functions.php';
 
 /**
  * The plugin install/uninstall test case.
  */
-require_once $dir . '/includes/wp-plugin-uninstall-unittestcase.php';
+require_once WP_PLUGIN_UNINSTALL_TESTER_DIR . '/includes/wp-plugin-uninstall-unittestcase.php';
 
 /**
  * Table exists constraint.
  */
-require_once $dir . '/includes/constraints/is-table-existant.php';
+require_once WP_PLUGIN_UNINSTALL_TESTER_DIR . '/includes/constraints/is-table-existant.php';
 
 /**
  * Table non-existant constraint.
  */
-require_once $dir . '/includes/constraints/table-is-non-existant.php';
+require_once WP_PLUGIN_UNINSTALL_TESTER_DIR . '/includes/constraints/table-is-non-existant.php';
 
 /**
  * No rows with prefix constraint.
  */
-require_once $dir . '/includes/constraints/no-rows-with-prefix.php';
-
-unset( $dir );
+require_once WP_PLUGIN_UNINSTALL_TESTER_DIR . '/includes/constraints/no-rows-with-prefix.php';
